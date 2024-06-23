066678edd99ad331aae31c4b9e7b51bc637f44c0
Tiago Grossi Hasuda <tiago.ghasuda@gmail.com>
2024-06-22 23:45:34 -0300

    refac: signin

--- src/auth/forgotPassword.ts
+++ src/auth/forgotPassword.ts
@@ -1,5 +1,12 @@
-export function handler(event) {
+export function handler(event, context, callback) {
   console.log({ event, test: "gjsdkfsjkfsad test" })
+  callback(null, {
+    statusCode: "200",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: "test",
+  })
 }
 
 export const httpMethod = "POST"
--- src/auth/resetPassword.ts
+++ src/auth/resetPassword.ts
@@ -1,5 +1,12 @@
-export function handler(event) {
+export function handler(event, context, callback) {
   console.log({ event })
+  callback(null, {
+    statusCode: "200",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: "test",
+  })
 }
 
 export const httpMethod = "POST"
