44697acd429db9a47291c9aedda051a4af61370c
Tiago Grossi Hasuda <tiago.ghasuda@gmail.com>
2024-06-24 14:30:23 -0300

    test: dynamodb

--- src/auth/signin.ts
+++ src/auth/signin.ts
@@ -3,9 +3,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 import {
   DynamoDBDocumentClient,
   GetCommand,
-  QueryCommand,
 } from "@aws-sdk/lib-dynamodb"
-
 
 // const params = {
 //   TableName: USERS_TABLE,
@@ -24,7 +22,7 @@ export async function handler(event, context, callback) {
   const USERS_TABLE = process.env.USERS_TABLE
   const client = new DynamoDBClient()
   const docClient = DynamoDBDocumentClient.from(client)
-  const command = new QueryCommand({ TableName: USERS_TABLE, AttributesToGet: ["id"] })
+  const command = new GetCommand({ TableName: USERS_TABLE, Key: { id: "test" } })
   const result = await docClient.send(command)
   console.log({ result })
   callback(null, {
